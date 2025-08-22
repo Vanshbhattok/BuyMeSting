da9adf038c63ce9267feb546ca830c4cef416479
Vansh bhatt <144117143+Vanshbhattok@users.noreply.github.com>
2025-08-22 13:05:14 +0530

    Updated the connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,8 +1,10 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const MONGODB_URI = 'mongodb://localhost:27017/buymesting';
+const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) throw new Error('MONGODB_URI is not defined');
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI environment variable in .env.local or Vercel dashboard");
+}
 
 let cached = global.mongoose || { conn: null, promise: null };
 global.mongoose = cached;
@@ -11,14 +13,16 @@ const connectDB = async () => {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      bufferCommands: false,
-    }).then((mongoose) => {
-      console.log(`MongoDB Connected: ${mongoose.connection.host}`);
-      return mongoose;
-    });
+    cached.promise = mongoose
+      .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        bufferCommands: false,
+      })
+      .then((mongoose) => {
+        console.log(`MongoDB Connected: ${mongoose.connection.host}`);
+        return mongoose;
+      });
   }
 
   cached.conn = await cached.promise;
